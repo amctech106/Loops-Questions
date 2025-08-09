59d2716ab2e8027168ee2bb10ef3bb7049051a72
amctech106 <121874402+amctech106@users.noreply.github.com>
2025-08-09 12:06:20 +0500

    Loops Question
    Solved some loops Questions

---
+++ Loops Practice/script.js
@@ -0,0 +1,79 @@
+//Print Numbers from 1 to 10 using for loop. 
+
+/* for (let i = 1; i<=10; i ++){
+    console.log(i);
+} */
+
+
+// print 10 1 using while loop 
+
+   /*  let i =10; 
+    while(i>0){
+       console.log(i)
+       i --; 
+    } */
+
+//print even numbers form 1 to 20 using for loops 
+
+/* for (let i= 1; i <=20; i ++){
+    if(i %2===0){
+    console.log(i);
+    }
+   
+}
+ */
+
+// print odd numbers from 1 to 15 using while loop 
+
+/* let i = 1; 
+
+while(i<=15){
+    i ++; 
+    if (i %2===1){
+ console.log(i); 
+    }
+} */
+
+//print multiplication table of 5 using for loop.
+
+/* for(let i =1; i <=10; i ++){
+    console.log(`5*${i}=${i*5}`);
+} */
+
+//find sum of numbers from 1 to 100 using loop.
+
+/* let sum =0;
+for(i = 1; i<=100; i++){
+    sum = sum +i; 
+    
+}
+console.log(sum); */
+
+///print numbers from 1 to 50 that are divisible by 3.
+
+/* for(let i= 1; i <=50; i ++){
+    if( i%3===0){
+        console.log(i)
+    }
+} */
+
+// ask the user for a number and print weather that number form 1 to that nuber are even and odd. 
+
+/* let number=prompt("Give a number");
+for(let i =1; i <=number; i ++){
+    if(i %2===0){
+     console.log(`${i} is even number`);   
+    }else{
+        console.log(`${i} is odd number`);
+    }
+} */
+
+// count how many numbers are from 1 to 100 divisible by 3 and 5. 
+
+/* for (let i =1; i <=100; i ++){
+    if(i %3===0 && i %5 === 0){
+        console.log(i);
+    }
+} */
+
+    
